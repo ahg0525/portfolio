60b78a44bf37109cd16e864d81b60298390633ae
withJungwonPark <jungwon.park@projectwith.io>
2024-08-14 17:20:59 +0900

    fix

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,5 @@
 import Layout from "../components/common/Layout"
-import AboutComponent from "../components/about"
+// import AboutComponent from "../components/about"
 import Intro1 from "../components/about/Intro1"
 import Intro2 from "../components/about/Intro2"
 
